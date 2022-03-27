7efb4aa3ebfecb78db731072dfeb83c24405a9db
L23-GCC <63116121+L23-GCC@users.noreply.github.com>
2022-03-27 13:32:25 -0400

    Testing main class
    This just shows how the board solver works with a manually created grid

--- main.java
+++ main.java
@@ -3,9 +3,21 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		CorralBoard b = new CorralBoard(3, 3);
-		b.fillBoard();
+		int[][] board = new int[3][3];
+		board[0][0] = 4;
+		board[0][1] = 3;
+		board[0][2] = 3;
+		board[1][0] = -1;
+		board[1][1] = 0;
+		board[1][2] = 0;
+		board[2][0] = -1;
+		board[2][1] = 0;
+		board[2][2] = 0;
+		
+		CorralBoard b = new CorralBoard(board);
+
 		System.out.println(b.toString());
+		System.out.println(b.isSolved());
 	}
 
 }
